8b4099a2853f39e5169d735b586980896a59bf9c
Anton <65621302+antonandrosov338@users.noreply.github.com>
2020-12-26 18:30:48 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -8,18 +8,21 @@ MAIN_CLASS=Program
 mkdir -p bin/obj bin/build
 
 #BUILD
-javac -classpath $LIBS/*.jar -d bin/obj $JAVA_FILES/*.java && \
+echo "build obj" && \
+javac -classpath "${LIBS}/*.jar" -d bin/obj $JAVA_FILES/*.java && \
 
 PACKAGE=`echo $PACKAGE | tr '/' '.'` && \
 
 echo main-class: $PACKAGE.$MAIN_CLASS >>bin/build/manifest.mf && \
-echo class-path: "${LIBS}/*.jar" >>bin/build/manifest.mf && \
+echo class-path: $LIBS/*.jar >>bin/build/manifest.mf && \
 
+echo "build jar" && \
 jar -cmf bin/build/manifest.mf bin/build/$MAIN_CLASS.jar -C bin/obj . && \
 
-rm bin/build/manifest.mf && \
+# rm bin/build/manifest.mf && \
 
 #START
+echo "start" && \
 java -jar bin/build/$MAIN_CLASS.jar
 
 exit 0
